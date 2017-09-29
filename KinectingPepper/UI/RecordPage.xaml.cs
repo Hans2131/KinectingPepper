124e7aef27bc616a7d32240d4ffdc23b069ecdbd
Enthoven, B. (13075837) <13075837@student.hhs.nl>
2017-09-29 16:39:16 +0200

    Buttons toegevoegd. Alle benodigdheden zijn er nu, ze kunnen nog verplaatsen.

--- KinectingPepper/UI/RecordPage.xaml.cs
+++ KinectingPepper/UI/RecordPage.xaml.cs
@@ -119,5 +119,15 @@ namespace Kinect_ing_Pepper.UI
         {
             Enum.TryParse<ECameraType>(cbxCameraType.SelectedValue.ToString(), out _selectedCamera);
         }
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void stopButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
