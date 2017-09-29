3e343f7a653d1a8f4c888533830509ab96b9c081
Bart Lanen <b.bart.lanen@gmail.com>
2017-09-29 12:26:30 +0200

    Basic class and added csvHelper package

---
+++ KinectingPepper/Business/CSV_saver.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kinect_ing_Pepper.Business
+{
+    public class CSV_saver
+    {
+        private DateTime startTime { get; set; }
+        private List<Tuple<Joint, TimeSpan, int>> jointList { get; set; }
+        private int frameCounter;
+        public CSV_saver()
+        {
+            frameCounter = 1;
+        }
+
+        public void saveCSV()
+        {
+        }
+
+        public void saveSkeletonFrame(Body body, DateTime datetime)
+        {
+            if (jointList == null)
+            {
+                jointList = new LinkedList<Tuple<Joint, TimeSpan>>;
+                startTime = datetime;
+            }
+            foreach (var joint in body.Joints)
+            {
+                var relativeTime = datetime - startTime;
+                Tuple tuple = new Tuple<Joint, TimeSpan>(joint, relativeTime, this.frameCounter);
+                jointList.Add(tuple);
+            }
+            frameCounter++;
+        }
+    }
+}
