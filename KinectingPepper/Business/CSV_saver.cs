e74e69c7c9d83c9e5cba7a4b766b39ba4b428a72
Bart Lanen <b.bart.lanen@gmail.com>
2017-10-10 13:26:50 +0200

    X1.1.1 save joints csv (#9)
    * Basic class and added csvHelper package
    
    * First implementation of body serialization (untested)
    
    * Adding tuple and csv module
    
    * Added simple check, only safe file when recordedframe exists
    
    * Made serialization of joints and timespan working
    
    * Added writing to csv, jointrow model for csv
    
    * Added datetime now to csv file
    
    * removed a } and formatting
    
    * Removed unused import & commented code & cleaned files
    
    * Working now with a nice ToString method
    
    * Removed unnecesary action to XML
    
    * Removed because it was already done

---
+++ KinectingPepper/Business/CSV_saver.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kinect_ing_Pepper.Models;
+using System.IO;
+using CsvHelper;
+
+namespace Kinect_ing_Pepper.Business
+{
+    public class CSV_saver
+    {
+        private List<JointRow> jList;
+        private int frameCounter;
+        private BodyWrapper trackedBody;
+        public CSV_saver()
+        {
+            frameCounter = 1;
+        }
+
+        public void saveCSV()
+        {
+            StreamWriter sw = new StreamWriter("joints_csv.csv");
+            var csv = new CsvWriter(sw);
+            csv.WriteRecords(jList);
+            csv.Dispose();
+        }
+
+        public void saveSkeletonFrame(BodyFrameWrapper bfw)
+        {
+            if (jList == null)
+            {
+                jList = new List<JointRow>();
+                trackedBody = bfw.TrackedBodies[0];
+            }
+            foreach (var bw in bfw.TrackedBodies)
+            {
+                foreach (var jw in bw.Joints)
+                {
+                    var jr = new JointRow(jw, bfw.RelativeTimeString, this.frameCounter, bw.TrackingId, jw.TrackingState);
+                    jList.Add(jr);
+                    if(frameCounter == 99)
+                    {
+                        Console.WriteLine(jr.ToString());
+                    }
+                }
+
+            }
+            this.frameCounter++;
+
+        }
+    }
+}
